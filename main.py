bc42d162beb86fd2a1a46f3be8ba96d55a1c2430
maxb87 <bretschneider.m@gmail.com>
2025-01-19 00:39:33 -0300

    Added player player sprite and hitbox

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import pygame
 from constants import *
+from circleshape import CircleShape
+from player import Player
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
-
+    
     while True: # main game loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
+                return
+        screen.fill("black")
+        player.draw(screen) 
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
+        dt = clock.tick(60) / 1000
+    
 if __name__ == "__main__":
     main()
