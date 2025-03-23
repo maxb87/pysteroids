e36e251375b5ed455545abbcacbeb085618a789b
maxb87 <bretschneider.m@gmail.com>
2025-03-22 23:16:58 -0300

    Broken the game implementing asteroidfield, might aswell restart

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,6 @@
 import pygame
+from circleshape import CircleShape
+
 
 class Asteroid(CircleShape):
   def __init__(self, x, y, radius):
@@ -8,3 +10,6 @@ class Asteroid(CircleShape):
 
   def update(self, dt):
     self.position += self.velocity * dt
+
+  def draw(self, screen):
+    pygame.draw.circle(screen, "white", self.position, self.radius, 2)
--- main.py
+++ main.py
@@ -2,31 +2,42 @@ import pygame
 from constants import *
 from circleshape import CircleShape
 from player import Player
-
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    dt = 0
+    
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-    updatable.add(player)
-    drawable.add(player)
-   
+    asteroids = pygame.sprite.Group()
+    
+    Asteroid.containers = (asteroids, updatable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    dt = 0
     
     while True: # main game loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        updatable.update(dt)
+
         screen.fill("black")
-        for sprite in drawable:
-            sprite.draw(screen)
-        for sprite in updatable:
-            sprite.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
+        # limit the frame rate to 60 FPS
         dt = clock.tick(60) / 1000
     
 if __name__ == "__main__":
